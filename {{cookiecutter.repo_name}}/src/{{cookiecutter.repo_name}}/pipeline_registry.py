988bb14a3a9e0e903f43c4d860243276b4305b35
Badreddine Ben Nouma <BenNoumaBadreddine@users.noreply.github.com>
2024-03-12 12:32:05 -0400

    Update pipeline_registry.py

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipeline_registry.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipeline_registry.py
@@ -1,8 +1,9 @@
-"""Project pipelines."""
 from typing import Dict
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+
+from email_ai.pipelines import raw, feature, intermediate, model, model_input, primary
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -12,5 +13,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     pipelines = find_pipelines()
+    pipelines["raw"] = raw.create_pipeline()
+    # pipelines["feature"] = feature.create_pipeline()
     pipelines["__default__"] = sum(pipelines.values())
     return pipelines
