91de568fd067292b0c80823581ee6a37aea79b22
Badreddine Ben Nouma <BenNoumaBadreddine@users.noreply.github.com>
2024-03-12 12:25:50 -0400

    Update pipeline.py

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipelines/intermediate/pipeline.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipelines/intermediate/pipeline.py
@@ -1,2 +1,15 @@
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
+
+from email_ai.extras.delete_me import print_message
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([
+        node(
+            func=print_message,
+            inputs="Hello, intermediate pipeline!",  # No inputs required
+            outputs=None,  # No outputs generated
+            name="print_intermediate_node"
+        )
+    ])
