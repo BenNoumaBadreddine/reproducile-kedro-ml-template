1cf0613f6b92cc471c05d25566633729cadea41f
Badreddine Ben Nouma <BenNoumaBadreddine@users.noreply.github.com>
2024-03-12 12:25:17 -0400

    Update pipeline.py

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipelines/feature/pipeline.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/pipelines/feature/pipeline.py
@@ -1,2 +1,16 @@
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
+
+from email_ai.extras.delete_me import print_message
+
+
+    
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([
+        node(
+            func=print_message,
+            inputs="Hello, feature pipeline!",  # No inputs required
+            outputs=None,  # No outputs generated
+            name="print_feature_node"
+        )
+    ])
